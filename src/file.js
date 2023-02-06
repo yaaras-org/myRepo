c0b3be79147541561aa58a13a4cd94115e5ef303
Yaara Shoham <yaara.shohamd@gmail.com>
2023-02-06 09:57:47 +0200

    Update file.js

--- src/file.js
+++ src/file.js
@@ -1,1 +1,10 @@
 console.log("Hi!");
+ console.log('testing');
+      console.log('testing');
+      console.log('testing');
+      console.log('testing');
+      console.log('testing');
+      console.log('testing');
+      console.log('testing');
+      console.log('testing');
+      console.log('testing');
