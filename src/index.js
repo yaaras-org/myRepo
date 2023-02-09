324b4413c793f73475d1057b8ae4511040d9ae9d
Yaara Shoham <yaara.shohamd@gmail.com>
2023-02-09 12:21:38 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 console.log('testing new file');
 console.log('print log for repo object within parser');
 console.log('test filtered author from suggested reviewer');
+console.log('test filtered author from suggested reviewer');
