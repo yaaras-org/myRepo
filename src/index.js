d0b45fa4a8c024b838e51e7da80dcf570e563225
Yaara Shoham <yaara.shohamd@gmail.com>
2023-04-27 11:31:32 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,3 +3,4 @@ console.log('print log for repo object within parser');
 console.log('test filtered author from suggested reviewer');
 console.log('test filtered author from suggested reviewer');
 console.log('test amplitude event updateAutomationRunSuccessCommonProvider');
+console.log('test amplitude event updateAutomationRunSuccessCommonProvider');
