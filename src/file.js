6f76b081ad7577c7b9d87ec11c7bfc473287723b
Yeela Lifshitz <52451294+yeelali14@users.noreply.github.com>
2023-06-21 12:10:22 +0300

    Update file.js

--- src/file.js
+++ src/file.js
@@ -6,3 +6,11 @@ console.log("Hi!");
 console.log("Hi!");
 
 console.log("Hi!");
+
+We're always running out of time
+(She's always running out of time)
+I'm always running out of time
+I'm always running out of time
+(She's always running out of time)
+Out of time
+Always running out of time
