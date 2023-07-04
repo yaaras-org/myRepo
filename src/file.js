c50a6c5e33ba9fb32dc4ff529ea309d6e810d035
Yaara Shoham <yaara.shohamd@gmail.com>
2023-07-04 14:04:51 +0300

    Running dry run

--- src/file.js
+++ src/file.js
@@ -15,3 +15,13 @@ console.log("(She's always running out of time)");
 console.log("Out of time");
 console.log("Always running out of time");
 console.log("It's me! Hi! I'm the problem it's me");
+
+
+console.log("We're always running out of time");
+console.log("(She's always running out of time");
+console.log("I'm always running out of time");
+console.log("I'm always running out of time");
+console.log("(She's always running out of time)");
+console.log("Out of time");
+console.log("Always running out of time");
+console.log("It's me! Hi! I'm the problem it's me");
