7fcb5fda608951759c57d651bfe1baf55dc5272d
Yaara Shoham <14977349+Yaarash@users.noreply.github.com>
2023-08-27 12:49:52 +0300

    Update file.js

--- src/file.js
+++ src/file.js
@@ -15,3 +15,10 @@ console.log("(She's always running out of time)");
 console.log("Out of time");
 console.log("Always running out of time");
 console.log("It's me! Hi! I'm the problem it's me");
+
+console.log('Vintage tee, brand new phone
+High heels on cobblestones
+When you are young, they assume you know nothing
+Sequin smile, black lipstick
+Sensual politics
+When you are young, they assume you know nothing');
