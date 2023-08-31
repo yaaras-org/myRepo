2e3deac71b992110aaaff840c981e16de6f69d23
Yaara Shoham <14977349+Yaarash@users.noreply.github.com>
2023-08-31 12:37:05 +0300

    Update file.js

--- src/file.js
+++ src/file.js
@@ -7,6 +7,8 @@ console.log("Hi!");
 
 console.log("Hi!");
 
+console.log("Hi!");
+
 console.log("We're always running out of time");
 console.log("(She's always running out of time");
 console.log("I'm always running out of time");
